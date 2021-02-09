ce7686cb67a758ec6da159001f97698eddfc64f9
Den Urakolouy <41019056+URAKOLOUY5@users.noreply.github.com>
2021-02-09 22:26:34 +0300

    Change `crosshair` to `hud_draw_crosshair`

--- sp/src/game/client/hud_crosshair.cpp
+++ sp/src/game/client/hud_crosshair.cpp
@@ -29,7 +29,7 @@
 // memdbgon must be the last include file in a .cpp file!!!
 #include "tier0/memdbgon.h"
 
-ConVar crosshair( "crosshair", "1", FCVAR_ARCHIVE );
+ConVar crosshair( "hud_draw_crosshair", "1", FCVAR_ARCHIVE );
 ConVar cl_observercrosshair( "cl_observercrosshair", "1", FCVAR_ARCHIVE );
 
 using namespace vgui;
