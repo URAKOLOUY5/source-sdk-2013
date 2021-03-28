e5d59e6b78dc1ab3d348eddcbe697cb51e762530
Den Urakolouy <41019056+URAKOLOUY5@users.noreply.github.com>
2021-03-28 14:11:37 +0300

    Add dbr_shareddeffs.h

---
+++ sp/src/game/shared/dbr/dbr_shareddeffs.h
@@ -0,0 +1,14 @@
+#ifndef DBR_SHAREDDEFS_H
+#define DBR_SHAREDDEFS_H
+
+//--------------
+// DBR SPECIFIC
+//--------------
+
+// Suppress jumping when player is ducking (like Portal 2)
+
+#define SUPRESS_JUMP_ON_DUCK
+
+#endif // DBR_SHAREDDEFS_H
+
+
